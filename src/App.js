227dfa12408eac56c857bb15615eb46e5f543699
Keith <klienert@madisoncollege.edu>
2024-04-21 10:45:07 -0500

    updated main page, removed logo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing...
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Testing in React...</h2>
       </header>
+      <main className="App-main">
+        <p>Main Section</p>
+      </main>
+      
     </div>
   );
 }
