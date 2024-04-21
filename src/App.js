12e9d0d2a50eefd6b49612828f0f9183bad3fff0
Keith <klienert@madisoncollege.edu>
2024-04-21 11:52:25 -0500

    cleanup project, remove logo, add new title

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ function App() {
 
   const [counter, setCounter] = useState(0);
 
-  const increment = () => {
+  const increment = () => {    
     setCounter(counter + 1);
   }
 
   const decrement = () => {
     setCounter(counter - 1);   
   }
+  
 
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Testing in React...</h2>
-      </header>      
+        <h2>Testing in React...</h2>        
+      </header>
+      <div className='App-description'>
+        <h3>Keith Lienert</h3>
+        <p>Cloud for Develpers - Project #4 <br />Spring 2024</p>
+        <small>It's not the prettiest example, but it demonstrates the CI/CD concept.</small>
+      </div>
       <div className='App-counter'>
         <h3>Counter Example</h3>
         <p className='counter'>Counter: {counter} </p>
