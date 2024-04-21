9bbedfc4b61d18880be9a8eeca605190bdd7c2c5
Keith <klienert@madisoncollege.edu>
2024-04-21 11:33:52 -0500

    added a basic counter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    setCounter(counter + 1);
+  }
+
+  const decrement = () => {
+    setCounter(counter - 1);   
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <h2>Testing in React...</h2>
-      </header>
-      <main className="App-main">
-        <p>Main Section</p>
-        <p>Holy crap, it worked!</p>
-        
-      </main>
+      </header>      
+      <div className='App-counter'>
+        <h3>Counter Example</h3>
+        <p className='counter'>Counter: {counter} </p>
+        <p className='counter-btns'>
+          <button onClick={increment} type="button">+</button>
+          <button onClick={decrement} type="button">-</button>
+        </p>
+      </div>
       
     </div>
   );
